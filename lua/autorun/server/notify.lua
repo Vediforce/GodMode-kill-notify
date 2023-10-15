44358ff2bd7639082de11a68bf23e4151040caad
Vediforce <86305600+Vediforce@users.noreply.github.com>
2023-10-15 22:10:11 +0300

    Update notify.lua

--- lua/autorun/server/notify.lua
+++ lua/autorun/server/notify.lua
@@ -1,10 +1,12 @@
-hook.Add("PlayerHurt", "GetAttacker", function(victim, attacker, damageTaken)
-    if IsValid(attacker) and attacker:IsPlayer() and attacker:HasGodMode() then
-        for _, ply in ipairs(player.GetAll()) do
-            if ply:IsAdmin() then
-                ply:Notify(attacker:Nick() .. " аттаковал " .. victim:Nick() .. "в годмоде!")
-            end
+hook.Add("PlayerHurt", "GetAttacker", function(victim, attacker, damageTaken, healthRemaining)
+    for _, ply in pairs(player.GetAll()) do
+        if ply:IsAdmin() and not attacker:IsAdmin() and attacker:HasGodMode() then
+            ply:PrintMessage(HUD_PRINTTALK, attacker:Nick() .. " аттаковал " .. victim:Nick() .. " в годмоде!")
         end
-        victim:SetHealth(victim:Health()+damageTaken)
+    end
+    if attacker:IsAdmin() then return end
+    if attacker:IsPlayer() and attacker:HasGodMode() then
+        print("123")
+        victim:SetHealth(victim:Health() + healthRemaining)
     end
 end)
